842b5966d862767640141bbeee0b42517faf7b49
Jiménez Verdejo Marco <mjimenezv2302@alumno.ipn.mx>
2025-10-21 10:56:32 -0600

    validacion de solo números

---
+++ frontend/validaciones.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputCampo = document.getElementById('qr-input');
+
+    inputCampo.addEventListener('keypress', function (evento) {
+        const charCode = (evento.which) ? evento.which : evento.keyCode;
+
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            evento.preventDefault();
+        }
+    });
+
+    inputCampo.addEventListener('paste', function (evento) {
+    
+        evento.preventDefault();
+
+        const textoPegado = (evento.clipboardData || window.clipboardData).getData('text');
+
+
+        const soloNumeros = textoPegado.replace(/[^0-9]/g, '');
+
+        this.value += soloNumeros;
+    });
+});
