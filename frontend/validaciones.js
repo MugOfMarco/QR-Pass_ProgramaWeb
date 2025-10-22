8ba6f6b8578d37a40be00fe0237bf89cf1cb8e44
Jiménez Verdejo Marco <mjimenezv2302@alumno.ipn.mx>
2025-10-21 23:21:59 -0600

    Cambio a las funciones de validacion de números y adiciond de la clase input-solo-numeros

--- frontend/validaciones.js
+++ frontend/validaciones.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const inputCampo = document.getElementById('soloNumeros-input');
+    const camposNumericos = document.querySelectorAll('.input-solo-numeros');
 
-    inputCampo.addEventListener('keypress', function (evento) {
+    function bloquearLetras(evento) {
         const charCode = (evento.which) ? evento.which : evento.keyCode;
-
         if (charCode > 31 && (charCode < 48 || charCode > 57)) {
             evento.preventDefault();
         }
-    });
+    }
 
-    inputCampo.addEventListener('paste', function (evento) {
-    
+    function limpiarPegado(evento) {
         evento.preventDefault();
-
         const textoPegado = (evento.clipboardData || window.clipboardData).getData('text');
-
-
         const soloNumeros = textoPegado.replace(/[^0-9]/g, '');
-
         this.value += soloNumeros;
+    }
+
+    camposNumericos.forEach(campo => {
+        campo.addEventListener('keypress', bloquearLetras);
+        campo.addEventListener('paste', limpiarPegado);
     });
 });
-
