f978c56a578930d65d4e5cba102fec15e75ec4fe
Jiménez Verdejo Marco <mjimenezv2302@alumno.ipn.mx>
2025-11-18 12:51:49 -0600

    adicion de archivos

---
+++ backend/crearUsuarios.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt');
+
+const passwordPlana = 'qwerty12345'; // <--- Pon aquí la contraseña que quieras
+const saltRounds = 10;
+
+bcrypt.hash(passwordPlana, saltRounds, function(err, hash) {
+    console.log("Copia este hash y úsalo en tu INSERT de SQL:");
+    console.log(hash);
+});
